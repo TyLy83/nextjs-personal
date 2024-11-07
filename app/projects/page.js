f7914b41dcccd6b1f942949d28285719c2485096
Ty Ly <ty.ly81@gmial.com>
2024-11-07 16:45:17 +1300

    fixed input.js

--- app/projects/page.js
+++ app/projects/page.js
@@ -8,9 +8,9 @@ export default function Projects() {
         <div className="py-8">
             <h1 className="mb-4 uppercase">projects</h1>
 
-            <Link href="/projects/modal">custom modal</Link>
+            <Link href="/projects/modal" className="block mb-4">custom modal</Link>
 
-            <Link href="/projects/input">custom inputs</Link>
+            <Link href="/projects/input" className="block mb-4">custom inputs</Link>
         </div>
     </>);
 
